32698443adf06bc84d2322a37c36439e2ef83b95
Ritesh Bucha <bucharitesh@gmail.com>
2024-09-07 13:03:07 +0530

    chore: minor fix

--- src/app/admin/dashboard/page.tsx
+++ src/app/admin/dashboard/page.tsx
@@ -5,14 +5,12 @@ const prisma = new PrismaClient();
 
 const Page = async () => {
   const buyers = await prisma.buyer.findMany();
-  const sellers = await prisma.seller.findMany();
 
   return (
     <div className="h-screen w-full px-12 py-8">
       {buyers && (
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
           {buyers.map((user: any) => {
-            if (user?.isApproved === "INITIALISED")
               return <UserCard user={user} />;
           })}
         </div>
